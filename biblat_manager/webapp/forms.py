00070df4823f440332ed4ee5ada7c9c8b171a1d4
Efrain Jacobo Avila Juarez <ejavilaj@dgb.unam.mx>
2018-09-13 15:35:30 -0500

    Agregado formulario de fasciculo.
     Agrege los cambios necesarios para presentar el formulario del fasciculo.
     Queda pendiente la integracion con el menu, que se encuentra del lado izquierdo.
    
     Cambios para hacer commit:
    	modificado:    biblat_manager/webapp/forms.py
    	modificado:    biblat_manager/webapp/main/views.py
    	nuevo archivo: biblat_manager/webapp/templates/forms/fasciculo.html
    	modificado:    biblat_manager/webapp/templates/left_panel.html

--- biblat_manager/webapp/forms.py
+++ biblat_manager/webapp/forms.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from flask_babelex import lazy_gettext as __
 from flask_wtf import FlaskForm
+
 import safe
 from wtforms import (
     StringField,
     PasswordField,
     BooleanField,
     validators,
-    ValidationError)
+    ValidationError,
+    IntegerField,
+    DateTimeField)
 
 
 def check_secure_password(form, field):
@@ -24,6 +27,7 @@ def check_secure_password(form, field):
     }
     if not strength.valid:
         raise ValidationError(messages[strength.message])
+
 
 
 class RegistrationForm(FlaskForm):
@@ -74,3 +78,37 @@ class PasswordForm(FlaskForm):
         check_secure_password
     ])
     confirm = PasswordField(__('Confirmar contraseña'))
+
+class FasciculoForm(FlaskForm):
+    revista = StringField(__('Revista'),[
+        validators.Length(max=150),
+        validators.DataRequired()
+    ])
+    volumen = IntegerField(__('Volumen'),[
+        validators.Length(max=15),
+    ])
+    numero = IntegerField(__('Numero'),[
+        validators.Length(max=15),
+    ])
+    anio = IntegerField(__('Año'),[
+        validators.Length(max=4),
+        validators.DataRequired()
+    ])
+    "**** Tengo duda de como manejar el mes en el formato, con nombre o con numero *****"
+    mes_inicial = IntegerField(__('Mes inicial'),[
+        validators.DataRequired()
+    ])
+    mes_final = IntegerField(__('Mes final'),[
+        validators.DataRequired()
+    ])
+    parte = StringField(__('Parte'),[
+        validators.Length(max=100),
+        validators.DataRequired()
+    ])
+    fecha_creacion = DateTimeField(__('Fecha de creacion'),[
+        validators.DataRequired()
+    ])
+    fecha_actualizacion = StringField(__('Fecha de actualizacion'),[
+        validators.Length(max=15),
+        validators.DataRequired()
+    ])
--- biblat_manager/webapp/main/views.py
+++ biblat_manager/webapp/main/views.py
@@ -15,7 +15,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from . import main
 from biblat_manager.webapp import babel, controllers
 from biblat_manager.webapp.forms import (
-    RegistrationForm, LoginForm, EmailForm, PasswordForm
+    RegistrationForm, LoginForm, EmailForm, PasswordForm, FasciculoForm
 )
 from biblat_manager.webapp.models import User
 from biblat_manager.webapp.utils import get_timed_serializer
@@ -301,3 +301,30 @@ def reset_with_token(token):
     }
     return render_template('auth/reset_with_token.html', **data)
 
+
+@main.route('/fasciculos/agregar', methods=['GET', 'POST'])
+@register_breadcrumb(main, '.fasciculos', __('Fasciculos'))
+@login_required
+def fasciculos():
+    form = FasciculoForm()
+    return render_template('forms/fasciculo.html', form=form)
+
+
+class BiblatSchemaAdminView(mongoengine.ModelView):
+    page_size = 20
+    can_create = False
+    can_edit = False
+    can_delete = False
+    create_modal = True
+    edit_modal = True
+    can_view_details = True
+
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class UserAdminView(BiblatSchemaAdminView):
+    can_create = True
+    can_edit = True
+    can_delete = False
+    page_size = 30
